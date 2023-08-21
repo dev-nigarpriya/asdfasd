93a459380f0452216a4ac7d07c9229a5aba5a13b
dev-nigarpriya <123937836+dev-nigarpriya@users.noreply.github.com>
2023-08-21 16:03:14 +0600

    asdfasd

---
+++ namta2/js/script.js
@@ -0,0 +1,22 @@
+// let box = document.querySelector("#box")
+// let addBtn = document.querySelector(".addBtn")
+// let result = document.querySelector(".result")
+
+
+// addBtn.addEventListener("click",function(){
+//  result.innerHTML = box.value
+// })
+
+/*let num = 2
+for(i=1;i<=num;i++){
+    for(j=i;j<=10;j++){
+        console.log(i + "X" +j+ "="+ j*i)
+    }
+}*/
+
+/*const num = 3;
+for(let i=num;i<=10;i++){
+    for(let j=1;j<=10;j++){
+        console.log(i+"X"+j+"="+i*j)
+    }
+}*/
